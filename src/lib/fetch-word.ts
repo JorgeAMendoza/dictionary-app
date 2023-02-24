a643c9bc5011b5dbac5ca9f34246960fbb1b08c7
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-02-24 14:10:55 -0600

    create function that calls api using axios and extract word information to form that will make it easier for it to be managed by react

--- src/lib/fetch-word.ts
+++ src/lib/fetch-word.ts
@@ -1,10 +1,49 @@
 import axios from 'axios';
 
+const grabAudio = (phonetics: WordResponse['phonetics']): string | null => {
+  for (const phon of phonetics) {
+    if (phon.audio !== '' && phon.audio.includes('us.mp3')) return phon.audio;
+  }
+  return null;
+};
+
 const fetchWord = async (word: string): Promise<WordInformation> => {
   try {
     const { data } = await axios.get<APIResponse>(
       `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
     );
+    const wordInformation = {
+      word: data[0].word,
+      phonetic: data[0].phonetic,
+      meanings: data[0].meanings,
+      audio: grabAudio(data[0].phonetics),
+    };
+
+    if (data.length === 1) return wordInformation;
+
+    const extraWords = data.slice(1);
+
+    const currentMeanings = [...wordInformation.meanings];
+
+    const extraMeanings: Meanings[] = [];
+
+    for (let i = 0; i < extraWords.length; i++) {
+      const currentMeaning = extraWords[i].meanings;
+      for (let j = 0; j < currentMeaning.length; j++)
+        extraMeanings.push(currentMeaning[j]);
+    }
+
+    for (const meaning of extraMeanings) {
+      if (
+        !currentMeanings.find(
+          (word) => meaning.partOfSeach !== word.partOfSeach
+        )
+      ) {
+        wordInformation.meanings.push(meaning);
+      }
+    }
+
+    return wordInformation;
   } catch (e: unknown) {
     if (axios.isAxiosError(e)) {
       const response = e.response?.data as ErrorResponse;
@@ -14,29 +53,6 @@ const fetchWord = async (word: string): Promise<WordInformation> => {
     }
     throw new Error('Something went wrong', { cause: 'Please try again' });
   }
-
-  // if data length is nothing, or the array is 0, then throw an error respone
-
-  // normalizing state, putting the state in a way that makes sense for us.
-  // grab the first word, and set it equal to an object with teh word Information type, or put it in a way
-
-  // grab word
-  // grab phonetic
-
-  // iterate over phonetics and find audio containing "us.mp3"
-  // if not there then set to null.
-
-  // grab meanings and set them.
-
-  // at this point we are done with the base.
-
-  // next if the data length is more than one, then iterate over the rest of the aray (slice)
-
-  // iterate/extract meaings,
-  // if the current part of speech already exist, ignore,
-  // else put it in the object.
-
-  // Watch for meanings, since its optional it should be fine to just extract and replace, and type guard for it.
 };
 
 export default fetchWord;
