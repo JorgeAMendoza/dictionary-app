c45c071a779212f322df06b8b0bbd87b576952e4
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-03-06 06:17:50 -0600

    wrap app around font context

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import FontProvider from './context/font-context';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <FontProvider>
+      <App />
+    </FontProvider>
   </React.StrictMode>
 );
