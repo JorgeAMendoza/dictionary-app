2eabcdd5d4d461e9030e61b7214e5b085f5d59cf
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-03-08 18:39:24 -0600

    implment global style which takes in the font context value to set the font on the body

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import NavBar from './components/NavBar/NavBar';
 import WordSearch from './components/WordSearch/WordSearch';
+import { useFontContext } from './hooks/context';
+import GlobalStyle from './styles/Global.styled';
 
 function App() {
+  const font = useFontContext();
   return (
     <div>
+      <GlobalStyle font={font} />
       <NavBar />
       <WordSearch />
     </div>
---
+++ src/styles/Global.styled.tsx
@@ -0,0 +1,67 @@
+import { createGlobalStyle } from 'styled-components';
+import { Fonts } from '../context/font-context';
+
+interface GlobalStyleProps {
+  font: Fonts;
+}
+
+const GlobalStyle = createGlobalStyle<GlobalStyleProps>`
+  html {
+    font-size: 62.5%;
+    box-sizing: border-box;
+  }
+  *,
+  *::after,
+  *::before {
+    box-sizing: inherit;
+    padding: 0;
+    margin: 0;
+    line-height: 1.4;
+    z-index:1;
+  }
+  body{
+    font-family: ${({ font }) =>
+      font === 'mono'
+        ? 'monospace'
+        : font === 'sans-serif'
+        ? 'sans-serif'
+        : 'serif'}, serif;
+    font-size: 1.6rem;
+    min-height: 100vh;
+    position:relative;
+    background-color: black;
+    color: var(--white);
+    padding-bottom:3rem;
+  } 
+  img,svg {
+    max-width: 100%;
+    display: block;
+  }
+  a{
+    color:white;
+  }
+  input {
+    font-family: inherit;
+    border: none;
+  }
+  ul,ol{
+    list-style: none;
+  }
+  p,
+  li,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    overflow-wrap: break-word;
+    hyphens: auto;
+  }
+  
+  #root, #__next {
+  isolation: isolate;
+}
+`;
+
+export default GlobalStyle;
