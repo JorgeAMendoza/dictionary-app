41a88ef07af61891d8e01ebb03a109908d78f7a7
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-03-13 09:17:49 -0500

    display error text for empty search query

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -7,24 +7,33 @@ interface SearchBarProps {
 
 const SearchBar = ({ setWord }: SearchBarProps) => {
   const [search, setSearch] = useState('keyboard');
+  const [error, setError] = useState('');
+
   return (
     <form
       aria-label="form for searching word"
       onSubmit={(e) => {
         e.preventDefault();
+        if (!search) {
+          setError("Whoops, can't be empty...");
+          return;
+        }
         setWord(search);
       }}
     >
-      <label>
-        <input
-          type="search"
-          placeholder="Search for any word..."
-          onChange={(event) => setSearch(event.target.value)}
-          onBlur={(event) => setSearch(event.target.value)}
-          value={search}
-          data-cy="searchBar"
-        />
-      </label>
+      <div>
+        <label>
+          <input
+            type="search"
+            placeholder="Search for any word..."
+            onChange={(event) => setSearch(event.target.value)}
+            onBlur={(event) => setSearch(event.target.value)}
+            value={search}
+            data-cy="searchBar"
+          />
+        </label>
+        {error ? <p>{error}</p> : null}
+      </div>
 
       <button
         aria-label="button to launch search for word"
