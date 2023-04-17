8df5faa6a7b8b58a3262cbd52b27d39f3d6f8dbd
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-04-17 06:19:15 -0500

    create file containting media query breakpoints

--- src/styles/utils/Container.styled.tsx
+++ src/styles/utils/Container.styled.tsx
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
+import device from './device';
 
 export const Container = styled.div`
-  width: min(88%, 73.7rem);
+  width: 88%;
   margin: 0 auto;
+
+  @media screen and (${device.tablet}) {
+    width: min(90%, 73.6rem);
+  }
 `;
