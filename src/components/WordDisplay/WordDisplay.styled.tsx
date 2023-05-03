4b1440ad6e65b1666cc17af9f0e0e60dd3ffd85b
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-05-03 06:33:09 -0500

    remove uncessary import

--- src/components/WordDisplay/WordDisplay.styled.tsx
+++ src/components/WordDisplay/WordDisplay.styled.tsx
@@ -1,4 +1,4 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 import device from '../../styles/utils/device';
 import { fadeIn } from '../../styles/utils/animations';
 
